5a49d787ee2a86e109f99ca85ef0f5b2c90c7405
廖宇浩 <taiyangniaoming@foxmail.com>
2018-08-14 16:12:11 +0800

    Add SafeArea support

--- README.md
+++ README.md
@@ -8,7 +8,14 @@ First, setSource at anywhere:
 import 'package:percent_helper/percent_helper.dart' as percent;
 
 void main() {
-  percent.setSource(1920, 1080);    // The design size you want
+  // The design size you want
+  // The map of safeArea is used with SafeArea Widget
+  percent.setSource(1920, 1080, safeArea: {
+      "left" : true,
+      "right" : true,
+      "top" : true,
+      "bottom" : true,
+    });    
   runApp(new MyApp());
 }
 ```
@@ -18,5 +25,5 @@ import 'package:percent_helper/percent_helper.dart';
 // Full screen
 new SizedBox(
             height: height(1920),
-            width: width(1080),
+            width: width(1080), 
 ```
--- lib/percent_helper.dart
+++ lib/percent_helper.dart
@@ -8,6 +8,7 @@ double _sourceHeight;
 double _sourceWidth;
 double _screenHeight;
 double _screenWidth;
+Map<String, bool> _safeArea;
 
 double height(num height) {
   checkSource();
@@ -21,16 +22,26 @@ double width(num width) {
 
 void checkSource() {
   if (_screenHeight == 0 || _sourceWidth == 0)
-    setSource(_sourceHeight, _sourceWidth);
+    setSource(_sourceHeight, _sourceWidth, safeArea: _safeArea);
 }
 
-
-setSource(num sourceHeight, num sourceWidth) {
-  var screenSize = MediaQueryData
-      .fromWindow(ui.window)
-      .size;
-  _screenHeight = screenSize.height;
-  _screenWidth = screenSize.width;
+setSource(num sourceHeight, num sourceWidth,
+    {Map<String, bool> safeArea = const {
+      "left": false,
+      "right": false,
+      "top": false,
+      "bottom": false,
+    }}) {
+  final mediaQueryData = MediaQueryData.fromWindow(ui.window);
+  final screenSize = mediaQueryData.size;
+  final safeAreaPadding = mediaQueryData.padding;
+  var ignoreHeight = safeArea['top'] ? safeAreaPadding.top : 0;
+  ignoreHeight += safeArea['bottom'] ? safeAreaPadding.bottom : 0;
+  var ignoreWidth = safeArea['left'] ? safeAreaPadding.left : 0;
+  ignoreWidth += safeArea['right'] ? safeAreaPadding.right : 0;
+  _screenHeight = screenSize.height - ignoreHeight;
+  _screenWidth = screenSize.width - ignoreWidth;
   _sourceHeight = sourceHeight.toDouble();
   _sourceWidth = sourceWidth.toDouble();
+  _safeArea = safeArea;
 }
